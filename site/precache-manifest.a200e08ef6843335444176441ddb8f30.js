71dd94f6d893f1f6e87162e433c0e8032a900cf4
Saul Quispe <saul9412@hotmail.com>
2023-03-08 21:51:06 -0500

    first deploy

---
+++ site/precache-manifest.a200e08ef6843335444176441ddb8f30.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54405061694e3cedbf5e4dc22eabe5ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7241a2ff93112f02baf5",
+    "url": "/static/css/main.b71ff71c.chunk.css"
+  },
+  {
+    "revision": "bdc44d3a8c17e4497e34",
+    "url": "/static/js/0.ba57d9c6.chunk.js"
+  },
+  {
+    "revision": "9312913abd3ec291e09a77758d4b839e",
+    "url": "/static/js/0.ba57d9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad231708e906748b4e8",
+    "url": "/static/js/1.84eba181.chunk.js"
+  },
+  {
+    "revision": "580256ae5f97366621b8",
+    "url": "/static/js/10.276b7dce.chunk.js"
+  },
+  {
+    "revision": "7454fb2351bb82e37bca",
+    "url": "/static/js/11.0ed4bf5a.chunk.js"
+  },
+  {
+    "revision": "604e3e9550829686d89e31a9b255da4e",
+    "url": "/static/js/11.0ed4bf5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bc76c3f08f7409d89d",
+    "url": "/static/js/12.c8eda683.chunk.js"
+  },
+  {
+    "revision": "1467cac85c9f6ed141fe",
+    "url": "/static/js/13.26a18920.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/static/js/13.26a18920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a89cae60c9d2c4f913",
+    "url": "/static/js/14.0e4b2162.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/14.0e4b2162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495a81e364c24f0a207f",
+    "url": "/static/js/15.6bbf42a1.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/15.6bbf42a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190a20acd62005cd4d09",
+    "url": "/static/js/16.f133fee3.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/16.f133fee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5664cc9483ad702f843",
+    "url": "/static/js/17.d941990e.chunk.js"
+  },
+  {
+    "revision": "1f16536060b59e5476b9",
+    "url": "/static/js/18.68e7ca23.chunk.js"
+  },
+  {
+    "revision": "77f37296b06ed76d77fd",
+    "url": "/static/js/4.9ced917f.chunk.js"
+  },
+  {
+    "revision": "2ba88f639a15be4a7f93bf30163a8ccf",
+    "url": "/static/js/4.9ced917f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a1dd8d3c24b573021e",
+    "url": "/static/js/5.770f10c1.chunk.js"
+  },
+  {
+    "revision": "d2b2b1c8f4bd9aa447e4",
+    "url": "/static/js/6.3c24e2ef.chunk.js"
+  },
+  {
+    "revision": "c3fe86c921e10e385406",
+    "url": "/static/js/7.b65df6d1.chunk.js"
+  },
+  {
+    "revision": "bbd453bc7ea34b5ff519",
+    "url": "/static/js/8.0c4c34f9.chunk.js"
+  },
+  {
+    "revision": "ac9a15dfc0924a827144",
+    "url": "/static/js/9.e366e77b.chunk.js"
+  },
+  {
+    "revision": "7241a2ff93112f02baf5",
+    "url": "/static/js/main.2575f3a3.chunk.js"
+  },
+  {
+    "revision": "c21d41384052c6777bf3",
+    "url": "/static/js/runtime-main.4ec9ee76.js"
+  },
+  {
+    "revision": "c76b33ca42c5730ab77f3341ce9764a7",
+    "url": "/static/media/USDC.c76b33ca.png"
+  },
+  {
+    "revision": "10f2f6b9092a3fe942957bc1d0c8f767",
+    "url": "/static/media/cemetery.10f2f6b9.png"
+  },
+  {
+    "revision": "f3331ee892233fc83932c9e5873c1523",
+    "url": "/static/media/crypto_tomb_bond.f3331ee8.svg"
+  },
+  {
+    "revision": "4d4f8ca6b4b157510401c5258739104e",
+    "url": "/static/media/crypto_tomb_cash.f2b44ef4.4d4f8ca6.png"
+  },
+  {
+    "revision": "f2b44ef4a644ec61342c200db3380871",
+    "url": "/static/media/crypto_tomb_cash.f2b44ef4.svg"
+  },
+  {
+    "revision": "14510c1a5487c2d5176fe427ef6bda7f",
+    "url": "/static/media/crypto_tomb_share.bf1a6c52.14510c1a.png"
+  },
+  {
+    "revision": "bf1a6c521de2fa79f8d24213632e648d",
+    "url": "/static/media/crypto_tomb_share.bf1a6c52.svg"
+  },
+  {
+    "revision": "5f33a495a08fa7b894087932ec386e7b",
+    "url": "/static/media/discord.5f33a495.svg"
+  },
+  {
+    "revision": "d8eaf3b25aed09659f2d497fb4072914",
+    "url": "/static/media/ftm_logo_blue.d8eaf3b2.svg"
+  },
+  {
+    "revision": "87c905bf444c3ba2ef82c9d970f5a6ed",
+    "url": "/static/media/home.87c905bf.png"
+  },
+  {
+    "revision": "042cb5a6cbac5284edd348e07126fbb8",
+    "url": "/static/media/masonry.042cb5a6.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "268bf98426e621fb6c1f3b6db9970d9d",
+    "url": "/static/media/pit.268bf984.png"
+  },
+  {
+    "revision": "b9559c2c1d7477268c82bf3a12058bc1",
+    "url": "/static/media/regulations_bg.b9559c2c.png"
+  },
+  {
+    "revision": "554ab6e67f0c2414db5263a53d0a0f11",
+    "url": "/static/media/shiba_logo.554ab6e6.svg"
+  },
+  {
+    "revision": "1b007b4a25fa4ed1acdf4b3269eafe11",
+    "url": "/static/media/spooky.1b007b4a.png"
+  },
+  {
+    "revision": "28146bd816fad4b71ed4294a177c7662",
+    "url": "/static/media/telegram.28146bd8.svg"
+  },
+  {
+    "revision": "6f664a7c6f545a35c76de3580ed5e369",
+    "url": "/static/media/tomb-usdc-lp.6f664a7c.png"
+  },
+  {
+    "revision": "84532366399427c196380a893e0eee72",
+    "url": "/static/media/tomb_ftm_lp.84532366.png"
+  },
+  {
+    "revision": "07de9070995e61f6f9852dc4cdadf1d8",
+    "url": "/static/media/tshare_ftm_lp.07de9070.png"
+  },
+  {
+    "revision": "08d2ade8e1a89d1326b8a84d73c4cc6b",
+    "url": "/static/media/twitter.08d2ade8.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  },
+  {
+    "revision": "503d2e2264092df45d4d9795b0996855",
+    "url": "/static/media/youtube.503d2e22.svg"
+  },
+  {
+    "revision": "d34dbeabec78fb7d11408819ea3d9678",
+    "url": "/static/media/zoo_logo.d34dbeab.svg"
+  }
+]);
